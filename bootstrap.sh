a0752a6306ef916c5483450d04e7d149fcb73a64
sanjeevk <devsecops.dba@gmail.com>
2018-09-04 22:13:37 +0000

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 #
 #
 function install_packages() {
-    echo "[INFO] Calling: yum install -y $@"
-    yum install -y $@ > /dev/null
+    echo "[INFO] Calling: yum install -y $$@"
+    yum install -y $$@ > /dev/null
 }
 #
 function attach_volume () {
@@ -12,24 +12,24 @@ function attach_volume () {
   instance_id=$2
   volume_id=$3
   cmd_output=$(aws ec2 attach-volume \
-        --device ${fs_device} \
-        --instance-id ${instance_id} \
-        --volume-id ${volume_id} \
+        --device $${fs_device} \
+        --instance-id $${instance_id} \
+        --volume-id$ $${volume_id} \
          2>&1)
    return_code=$?
-   echo "${return_code}:${cmd_output}"
+   echo "$${return_code}:$${cmd_output}"
 }
 
 #
 function get_volume_status() {
   volume_id=$1
   cmd_output=$(aws ec2 describe-volumes \
-        --volume-ids ${volume_id} \
+        --volume-ids $${volume_id} \
         --query Volumes[].State \
         --output text \
          2>&1)
    return_code=$?
-   echo "${return_code}:${cmd_output}"
+   echo "$${return_code}:$${cmd_output}"
 }
 
 #
@@ -41,13 +41,13 @@ function create_volume() {
   pratice_area=$5
   cmd_output=$(aws ec2 create-volume \
         --availability-zone ${avail_zone} \
-        --size ${size} \
+        --size $${size} \
         --volume-type gp2 \
         --region ${region} \
-        --tag-specifications "ResourceType=volume,Tags=[{Key=PracticeArea,Value=$practice_area},{Key=Name,Value=sanjeevk_rdbms_ebs_vol},{Key=DeviceName,Value=$fs_device}]")
+        --tag-specifications "ResourceType=volume,Tags=[{Key=PracticeArea,Value=$$practice_area},{Key=Name,Value=sanjeevk_rdbms_ebs_vol},{Key=DeviceName,Value=$$fs_device}]")
    return_code=$?
    sleep 30
-   echo "${return_code}:${cmd_output}"
+   echo "$${return_code}:$${cmd_output}"
 }
 #
 function get_volume_id() {
@@ -59,22 +59,22 @@ function get_volume_id() {
         --region ${region} \
         --filters \
             Name=availability-zone,Values=${avail_zone} \
-            Name=tag:DeviceName,Values=${fs_device} \
-            Name=tag:PracticeArea,Values=${practice_area} \
+            Name=tag:DeviceName,Values=$${fs_device} \
+            Name=tag:PracticeArea,Values=$${practice_area} \
         --query "Volumes[*].{ID:VolumeId}" \
         --output text \
          2>&1)
    return_code=$?
-   echo "${return_code}:${cmd_output}"
+   echo "$${return_code}:$${cmd_output}"
 }
 
 function get_instance_id() {
    instance_id=`curl -s -m 30 'http://169.254.169.254/latest/meta-data/instance-id'`
-   if [[ ${instance_id} == *"Not Found"* ]] ; then
+   if [[ $${instance_id} == *"Not Found"* ]] ; then
       echo "instance id is not found.exiting script."
       exit 1
    fi
-   echo ${instance_id}
+   echo $${instance_id}
 }
 #
 # main
@@ -98,68 +98,68 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 device_list=("/dev/xvdb|20|/u01" "/dev/xvdc|20|none")
 instance_id=`echo $(get_instance_id)`
 #
-for device in "${device_list[@]}"
+for device in "$${device_list[@]}"
 do
   #
-  fs_device=`echo $device | awk -F"|" '{print $1}'`
-  size=`echo $device | awk -F"|" '{print $2}'`
-  fs_mount=`echo $device | awk -F"|" '{print $3}'`
-  echo "fs_device:${fs_device}"
-  echo "size:${size}"
-  echo "fs_mount:${fs_mount}"
+  fs_device=`echo $$device | awk -F"|" '{print $1}'`
+  size=`echo $$device | awk -F"|" '{print $2}'`
+  fs_mount=`echo $$device | awk -F"|" '{print $3}'`
+  echo "fs_device:$${fs_device}"
+  echo "size:$${size}"
+  echo "fs_mount:$${fs_mount}"
 
   # get volume_id for fs_device
-  result=$(get_volume_id "$region" "$avail_zone" "$fs_device" "$practice_area")
-  return_code=`echo $result |awk  -F":" '{print $1}'`
-  ebs_volume_id=`echo $result |awk -F":" '{print $2}'`
+  result=$$(get_volume_id "$region" "$avail_zone" "$$fs_device" "$$practice_area")
+  return_code=`echo $$result |awk  -F":" '{print $1}'`
+  ebs_volume_id=`echo $$result |awk -F":" '{print $2}'`
 
   # if aws cmd succeeds and volid does not exist create it
-  if [[ ($return_code -eq 0) && ( -z "$ebs_volume_id" ) ]]; then
-    result=$(create_volume "$region" "$avail_zone" "$size" "$fs_device" "$practice_area")
-    return_code=`echo $result |awk  -F":" '{print $1}'`
-    ebs_volume_id=`echo $result |awk -F":" '{print $2}'`
-    if [[ ($return_code -eq 0) && ( -n "$ebs_volume_id" )]]; then
-      echo "Volume for ${fs_device} was newly created with volume_id:${ebs_volume_id}"
+  if [[ ($$return_code -eq 0) && ( -z "$$ebs_volume_id" ) ]]; then
+    result=$$(create_volume "$region" "$avail_zone" "$$size" "$$fs_device" "$$practice_area")
+    return_code=`echo $$result |awk  -F":" '{print $1}'`
+    ebs_volume_id=`echo $$result |awk -F":" '{print $2}'`
+    if [[ ($$return_code -eq 0) && ( -n "$$ebs_volume_id" )]]; then
+      echo "Volume for $${fs_device} was newly created with volume_id:$${ebs_volume_id}"
     fi
-  elif [[ ($return_code -eq 0) && ( -n "$ebs_volume_id" ) ]]; then
-    echo "Volume for ${fs_device}  already exists  with volume_id:${ebs_volume_id}"
+  elif [[ ($$return_code -eq 0) && ( -n "$$ebs_volume_id" ) ]]; then
+    echo "Volume for $${fs_device}  already exists  with volume_id:$${ebs_volume_id}"
   else
     echo "Issue with aws cmd. Please check. exiting"
     exit 1
   fi
 
   # get volume status for given vol_id
-  result=$(get_volume_status "$ebs_volume_id")
-  return_code=`echo $result |awk  -F":" '{print $1}'`
-  ebs_volume_state=`echo $result |awk -F":" '{print $2}'`
+  result=$$(get_volume_status "$$ebs_volume_id")
+  return_code=`echo $$result |awk  -F":" '{print $1}'`
+  ebs_volume_state=`echo $$result |awk -F":" '{print $2}'`
 
   # if aws cmd succeeds and volstatus is not attached
-  if [[ ($return_code -eq 0) && ( ${ebs_volume_state} == "available" ) ]]; then
-    echo "Volume for ${fs_device} has status: ${ebs_volume_state}"
-    result=$(attach_volume "$fs_device" "$instance_id" "$ebs_volume_id")
-    return_code=`echo $result |awk  -F":" '{print $1}'`
-    output=`echo $result |awk -F":" '{print $2}'`
+  if [[ ($$return_code -eq 0) && ( $${ebs_volume_state} == "available" ) ]]; then
+    echo "Volume for $${fs_device} has status: $${ebs_volume_state}"
+    result=$(attach_volume "$$fs_device" "$$instance_id" "$$ebs_volume_id")
+    return_code=`echo $$result |awk  -F":" '{print $1}'`
+    output=`echo $$result |awk -F":" '{print $2}'`
 
-    if [[ ($return_code -eq 0)]]; then
-       echo "Volume "$ebs_volume_id" with device "$fs_device" has been attached to instance "$instance_id" successfully"
+    if [[ ($$return_code -eq 0)]]; then
+       echo "Volume "$$ebs_volume_id" with device "$$fs_device" has been attached to instance "$$instance_id" successfully"
     else
     echo "Issue with aws cmd. Please check. exiting"
     fi
   else
-    echo "Volume for ${fs_device} has status: ${ebs_volume_state}"
+    echo "Volume for $${fs_device} has status: $${ebs_volume_state}"
   fi
 
   #
   # Format /dev/nnn> if it does not contain a partition yet
-  if [[ ${fs_mount} != "none" ]]; then
-  has_partition=`sudo file -b -s ${fs_device}`
-  echo "has_partition:${has_partition}"
-    if [ "${has_partition}" == "data" ]; then
-      sudo mkfs -t ext4 ${fs_device}
-      sudo mkdir -p ${fs_mount}
-      sudo mount ${fs_device} ${fs_mount}
+  if [[ $${fs_mount} != "none" ]]; then
+  has_partition=`sudo file -b -s $${fs_device}`
+  echo "has_partition:$${has_partition}"
+    if [ "$${has_partition}" == "data" ]; then
+      sudo mkfs -t ext4 $${fs_device}
+      sudo mkdir -p $${fs_mount}
+      sudo mount $${fs_device} $${fs_mount}
       # Persist the volume in /etc/fstab so it gets mounted again
-      echo "${fs_device} ${fs_mount} ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+      echo "$${fs_device} $${fs_mount} ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
     fi
   fi
 done
